e677dbd83e9cc9d81418a6293f79a6d20d3fe08c
genggguanghao <15103785516@163.com>
2019-01-30 18:08:48 +0800

    ssr

---
+++ src/routes/animal.vue
@@ -0,0 +1,20 @@
+<!-- App.vue -->
+<template>
+      <div>
+            <p>我是animal页面</p>
+      </div>
+</template>
+
+
+<script>
+export default {
+      mounted() {
+
+      }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/routes/people.vue
@@ -0,0 +1,20 @@
+<!-- App.vue -->
+<template>
+      <div>
+            <p>我是peple页面</p>
+      </div>
+</template>
+
+
+<script>
+export default {
+      mounted() {
+
+      }
+}
+</script>
+
+
+<style scoped>
+
+</style>
